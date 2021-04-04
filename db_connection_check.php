f94ef77fcf810579a660e6e6690e61fed351290f
abigail432 <54914351+abigail432@users.noreply.github.com>
2021-04-04 12:51:59 -0400

    Db connection

--- db_connection_check.php
+++ db_connection_check.php
@@ -5,6 +5,15 @@ $conn = OpenCon();
 
 echo "Connected Successfully";
 
+$result=mysqli_query($conn,"select * from users");
+mysqli_query($conn,"insert into users (Username, Password) values('Quack', 'DuckDuckGoose')");
+
+
+while($row=mysqli_fetch_array($result))
+{
+         echo $row['Username'].' '.$row['Password'].'<br/>';
+ }
+
 CloseCon($conn);
 
 ?>
