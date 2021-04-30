027f8a94fdacffcbd741b79c49d54546a597e4d8
abigail432 <54914351+abigail432@users.noreply.github.com>
2021-04-30 15:43:32 -0400

    Remove hard code info

--- authenticatelogin2.php
+++ authenticatelogin2.php
@@ -3,20 +3,31 @@ include 'db_connection.php';
 
 $conn = OpenCon();
 
-$username = "arelliott";
-$password = "andiloop";
-$result=mysqli_query($conn,"select * from users");
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+}
 
-while($row=mysqli_fetch_array($result))
-{
-    if($row['Password']==$password && $row['Username']==$username){
-        echo "password correct".'<br/>';
-        header("Location: successful_login.php");
-    }   
-    else{
-        echo "alert('Password incorrect')";
-    }
- }
+$results=mysqli_query($conn,"select * from users");
+
+foreach($results as $result){
+    if(($result['Username'] == $username) && ($result['Password'] == $password)) {
+            header("Location: successful_login.php");
+        }
+}
+
+echo "wrong info";
+
+// while($row=mysqli_fetch_array($result))
+// {
+//     if($row['Password']==$password && $row['Username']==$username){
+//         echo "password correct".'<br/>';
+//         //header("Location: successful_login.php");
+//     }   
+//     else{
+//         echo "alert('Password incorrect')";
+//     }
+//  }
 
 CloseCon($conn);
 
